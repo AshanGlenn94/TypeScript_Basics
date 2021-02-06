dac5b0538e6bb3b6eadfbbae3820e5f72a2cf6bb
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-06 16:06:39 -0600

    void and undefined return in TS

--- app.ts
+++ app.ts
@@ -1,33 +1,22 @@
-type Combinable = number | string;
-type ConversionDescription = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescription
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  //   if (resultConversion === "as-number") {
-  //     return +result;
-  //   } else {
-  //     return result.toString();
-  //   }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+//USed for the functions which does not return anything
+function printResult(num: number): void {
+  console.log("Result: " + num);
+}
 
-const combineStringAges = combine("30", "26", "as-number");
-console.log(combineStringAges);
+//Same as above but can used with return keyword too
+function printResult(num: number): void {
+  console.log("Result: " + num);
+  return;
+}
 
-const combinedNames = combine("Ashan", "Glenn", "as-text");
-console.log(combinedNames);
+//Can use undefined in TS as well but have to end with return keyword, just for demo purpose only
+function printResult(num: number): undefined {
+  console.log("Result: " + num);
+  return;
+}
+
+printResult(add(5, 12));
