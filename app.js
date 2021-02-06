fe24b716f3601bb64470745c3b63da5dc0d17ca0
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-06 15:13:34 -0600

    New type "tuple" with TypeScript
    Leraned about new data type "tuple" which does now include in JS but in some other languages. Anyhow TS allow to use tuple which gives the meaning of pre defined lenth and type of array.

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+// const person: {
+//   name: string;
+//   age: number;
+// }
 var person = {
     name: "Ashan",
-    age: 26
+    age: 26,
+    hobbies: ["DIY Projects", "Cooking", "Travelling"],
+    role: [2, "Developer"]
 };
+// person.role.push('admin');
+// person.role[1] = 10;
+var favouriteActivities;
+favouriteActivities = ["DIY Projects"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -2,13 +2,26 @@
 //   name: string;
 //   age: number;
 // }
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Ashan",
   age: 26,
   hobbies: ["DIY Projects", "Cooking", "Travelling"],
+  role: [2, "Developer"],
 };
+
+// person.role.push('admin');
+// person.role[1] = 10;
 
 let favouriteActivities: string[];
 favouriteActivities = ["DIY Projects"];
 
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
