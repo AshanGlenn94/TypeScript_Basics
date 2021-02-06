cf848ec9a9327b90819eb51d6706e65187deb279
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-06 16:15:37 -0600

    Function types with TS
    Using this we can specify which function we can use with specific data types

--- app.js
+++ app.js
@@ -5,3 +5,22 @@ function printResult(num) {
     console.log("Result: " + num);
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+//combineValues = 5;
+console.log(combineValues(8, 5));
+// //USed for the functions which does not return anything
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
+// //Same as above but can used with return keyword too
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+//   return;
+// }
+// //Can use undefined in TS as well but have to end with return keyword, just for demo purpose only
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
--- app.ts
+++ app.ts
@@ -2,21 +2,32 @@ function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
-//USed for the functions which does not return anything
-function printResult(num: number): void {
+function printResult(num: number) {
   console.log("Result: " + num);
-}
-
-//Same as above but can used with return keyword too
-function printResult(num: number): void {
-  console.log("Result: " + num);
-  return;
-}
-
-//Can use undefined in TS as well but have to end with return keyword, just for demo purpose only
-function printResult(num: number): undefined {
-  console.log("Result: " + num);
-  return;
 }
 
 printResult(add(5, 12));
+
+let combineValues: (x: number, y: number) => number;
+combineValues = add;
+// combineValues = printResult;
+//combineValues = 5;
+
+console.log(combineValues(8, 5));
+
+// //USed for the functions which does not return anything
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+// }
+
+// //Same as above but can used with return keyword too
+// function printResult(num: number): void {
+//   console.log("Result: " + num);
+//   return;
+// }
+
+// //Can use undefined in TS as well but have to end with return keyword, just for demo purpose only
+// function printResult(num: number): undefined {
+//   console.log("Result: " + num);
+//   return;
+// }
