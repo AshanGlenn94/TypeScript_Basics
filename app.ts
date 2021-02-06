4d56f3b63f5d1243f82a008130d322d0fe1e9cdd
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-06 09:59:23 -0600

    Array types with TypeScript

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// }
+const person = {
   name: "Ashan",
   age: 26,
+  hobbies: ["DIY Projects", "Cooking", "Travelling"],
 };
+
+let favouriteActivities: string[];
+favouriteActivities = ["DIY Projects"];
 
 console.log(person.name);
