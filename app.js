440a6ce340dd87d9e496dce4d9a658f7fe3debf5
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-06 16:22:38 -0600

    Adding callback funtion
    By adding a callback function which does not return anything (void) we can use that to print that specific outputn without caring about the type of that callback function. It can be useful in some scenarios.

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
 var combineValues;
 combineValues = add;
 // combineValues = printResult;
 //combineValues = 5;
 console.log(combineValues(8, 5));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
 // //USed for the functions which does not return anything
 // function printResult(num: number): void {
 //   console.log("Result: " + num);
--- app.ts
+++ app.ts
@@ -6,6 +6,11 @@ function printResult(num: number) {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResult(add(5, 12));
 
 let combineValues: (x: number, y: number) => number;
@@ -14,6 +19,10 @@ combineValues = add;
 //combineValues = 5;
 
 console.log(combineValues(8, 5));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
 
 // //USed for the functions which does not return anything
 // function printResult(num: number): void {
