bff96cad2c35deee6aedcfbee6485c0f7723a0d4
Ashan Glenn Seneviratne <ashanglenn1994@gmail.com>
2021-02-06 17:06:05 -0600

    "never" type
    Ex: In this case I have thrown a custom error with a message and an error code. However when we throw an error it does not return anything at all. We can check it simply trying to console it. But like in the most cases it does not even print "undefined" even. So this is the great place for us to define it as "never" since the function never returns anything forever.

--- app.js
+++ app.js
@@ -5,3 +5,7 @@ userInput = "Ashan";
 if (typeof userInput === "string") {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("An error occured", 500);
